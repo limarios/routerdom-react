5d810ecdf2c933edbc07c4ac7c602432aea55491
Matheus Alcance <limarios18@outlook.com>
2025-04-29 16:33:12 -0300

    ok

---
+++ src/paginas/Inicio/index.js
@@ -0,0 +1,23 @@
+import styles from './Inicio.module.css'
+import Banner from 'componentes/Banner'
+import Post from 'componentes/Post'
+
+import posts from 'json/posts.json'
+
+export default function Index(){
+    return (
+        <main>
+            <Banner />
+
+            <ul className={styles.posts}>
+                {posts.map( (post) => (
+                    <li key={post.id}>
+                        <Post post={post} />
+                    </li>
+                ) )}
+
+            </ul>
+        </main>
+        
+    )
+}
