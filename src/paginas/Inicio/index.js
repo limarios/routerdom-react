b42b1053d66706e2927834f2f0950315ea842744
Matheus Alcance <limarios18@outlook.com>
2025-04-30 16:44:42 -0300

    Remover componente Banner da página Inicial e simplificar a estrutura do JSX

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,23 +1,17 @@
 import styles from './Inicio.module.css'
-import Banner from 'componentes/Banner'
 import Post from 'componentes/Post'
 
 import posts from 'json/posts.json'
 
 export default function Index(){
     return (
-        <main>
-            <Banner />
-
-            <ul className={styles.posts}>
-                {posts.map( (post) => (
-                    <li key={post.id}>
-                        <Post post={post} />
-                    </li>
-                ) )}
-
-            </ul>
-        </main>
+        <ul className={styles.posts}>
+            {posts.map( (post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            ) )}
+        </ul>
         
     )
 }
